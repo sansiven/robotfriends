fe809e92fc3d40f53e76fc70046b5170ee00a630
Sabin <sansiven@gmail.com>
2018-04-15 11:33:16 +0545

    added first tutorial

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import {robots} from './robots';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css';
+
+
+class App extends Component {
+
+	constructor(){
+		super();
+		this.state= {
+			robots: [],
+			searchfield:'',
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> response.json())
+		.then(users => this.setState({robots:users}))		
+	}
+
+	onSearchchange =(event) => {
+		this.setState({searchfield: event.target.value});
+		//console.log(event.target.value);
+	};
+
+	render() {
+
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		if (this.state.robots.length === 0){
+			return <h1>Loading</h1>	
+		} 
+		else{
+			return (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange= {this.onSearchchange}/>
+					<Scroll>
+						<CardList robots = {filteredRobots}/>
+					</Scroll>
+				</div>
+			)
+		}
+	}
+}
+export default App;
